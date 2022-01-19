d392cd87aaee3e060245a51d2efb65659dfad200
Trofimov <mikletrof@gmail.com>
2022-01-19 10:52:21 +0300

    initial commit

---
+++ src/main/java/ru/trofimom/coffeemakerrestservice/CoffeemakerRestServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.trofimom.coffeemakerrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeemakerRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeemakerRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/trofimom/coffeemakerrestservice/CoffeemakerRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.trofimom.coffeemakerrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeemakerRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
