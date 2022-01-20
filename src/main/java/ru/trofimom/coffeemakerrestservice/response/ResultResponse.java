5be9cbce77ac4703778c499b56c1dfec280ca9e6
Trofimov <mikletrof@gmail.com>
2022-01-20 14:43:23 +0300

    added @JsonInclude

--- src/main/java/ru/trofimom/coffeemakerrestservice/response/ResultResponse.java
+++ src/main/java/ru/trofimom/coffeemakerrestservice/response/ResultResponse.java
@@ -1,18 +1,28 @@
 package ru.trofimom.coffeemakerrestservice.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import ru.trofimom.coffeemakerrestservice.model.CoffeeMaker;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResultResponse {
 
-   private String message;
+    private String message;
+
+    private CoffeeMaker coffeeMaker;
 
     public ResultResponse(String message) {
         this.message = message;
+    }
+
+    public ResultResponse(CoffeeMaker coffeeMaker) {
+        this.coffeeMaker = coffeeMaker;
     }
 
     public String getMessage() {
         return message;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public CoffeeMaker getCoffeeMaker() {
+        return coffeeMaker;
     }
 }
